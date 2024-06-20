235b079eb62d38c5314b1389d344a3241fc08937
Anmol992894 <av992894@gmail.com>
2024-06-20 12:12:37 +0530

    Deploy

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGOBD_URL: "mongodb+srv://av992894:zE4zpO44dRDRPsWM@cluster0.y1idp1c.mongodb.net/ECom?retryWrites=true&w=majority&appName=Cluster0",
+    JWT_SECRET: "hafsfffgg72846285jjjkh54646467hh"
+}
